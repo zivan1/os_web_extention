24083dd99ed1799704d5c45116f131f70704ebbb
zivan1 <zivan1@ya.ru>
2018-12-04 23:15:42 +0300

    Create MEMBERSHIP_WHITELIST.js

---
+++ MEMBERSHIP_WHITELIST.js
@@ -0,0 +1,11 @@
+window.OPENSHIFT_CONSTANTS.MEMBERSHIP_WHITELIST = [
+  "admin",
+  "basic-user",
+  "edit",
+  "system:deployer",
+  "system:image-builder",
+  "system:image-puller",
+  "system:image-pusher",
+  "view",
+  "viewer-dev"
+];
