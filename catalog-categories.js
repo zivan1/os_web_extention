5cc28cd47fe44eb2ad6fdfea1e44f2aaf5de11d1
zivan1 <zivan1@ya.ru>
2018-12-10 14:47:41 +0300

    Create catalog-categories.js

---
+++ catalog-categories.js
@@ -0,0 +1,16 @@
+// Find the Languages category.
+var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
+                      { id: 'languages' });
+// Add Go as a new subcategory under Languages.
+category.subCategories.splice(2,0,{ // Insert at the third spot.
+  // Required. Must be unique.
+  id: "test",
+  // Required.
+  label: "TEST",
+  // Optional. If specified, defines a unique icon for this item.
+  icon: "icon-go-gopher",
+  // Required. Items matching any tag will appear in this subcategory.
+  tags: [
+    "test"
+  ]
+});
