4367f1b96a36303ec2e131a109f065216f18a841
zivan1 <zivan1@ya.ru>
2018-12-10 15:10:06 +0300

    Update catalog-categories.js

--- catalog-categories.js
+++ catalog-categories.js
@@ -1,16 +1,33 @@
-// Find the Languages category.
-var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
-                      { id: 'languages' });
-// Add Go as a new subcategory under Languages.
-category.subCategories.splice(2,0,{ // Insert at the third spot.
+window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   // Required. Must be unique.
-  id: "test",
-  // Required.
-  label: "TEST",
-  // Optional. If specified, defines a unique icon for this item.
-  icon: "icon-go-gopher",
-  // Required. Items matching any tag will appear in this subcategory.
-  tags: [
-    "httpd"
+  id: "featured",
+  // Required
+  label: "MYTEST",
+  subCategories: [
+    {
+      // Required. Must be unique.
+      id: "go",
+      // Required.
+      label: "Go",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-go-gopher",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "go",
+        "golang"
+      ]
+    },
+    {
+      // Required. Must be unique.
+      id: "test",
+      // Required.
+      label: "TEST",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-jenkins",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "httpd"
+      ]
+    }
   ]
 });
